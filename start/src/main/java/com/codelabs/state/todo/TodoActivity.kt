591254c476e2cb6e63565ce26a98b76655e00b85
Jean Tadebois <jpm.tadebois@gmail.com>
2021-05-08 14:09:35 +0400

    Add complete code for section Memory in Compose

--- start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
+++ start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
@@ -25,7 +25,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import com.codelabs.state.ui.StateCodelabTheme
-import com.codelabs.state.util.generateRandomTodoItem
 
 class TodoActivity : AppCompatActivity() {
 
