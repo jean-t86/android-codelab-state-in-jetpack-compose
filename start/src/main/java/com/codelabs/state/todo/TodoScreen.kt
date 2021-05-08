7e692e104d87882f25391b7cc858efa0a586ac21
Jean Tadebois <jpm.tadebois@gmail.com>
2021-05-08 15:43:14 +0400

    Add complete code for Dynamic UI based on state

--- start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
+++ start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
@@ -131,40 +131,42 @@ fun PreviewTodoRow() {
 }
 
 @Composable
-fun TodoInputTextField(
-    text: String,
-    onTextChange: (String) -> Unit,
-    modifier: Modifier
-) {
-    TodoInputText(text, onTextChange, modifier)
-}
-
-@Composable
 fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
     val (text, setText) = remember { mutableStateOf("") }
-    // onItemComplete is an event will fire when an item is completed by the user
+    val (icon, setIcon) = remember { mutableStateOf(TodoIcon.Default) }
+    val iconsVisible = text.isNotBlank()
+    val submit = {
+        onItemComplete(TodoItem(text, icon))
+        setIcon(TodoIcon.Default)
+        setText("")
+    }
     Column {
         Row(
             Modifier
                 .padding(horizontal = 16.dp)
                 .padding(top = 16.dp)
         ) {
-            TodoInputTextField(
+            TodoInputText(
                 text,
                 setText,
                 Modifier
                     .weight(1f)
-                    .padding(end = 8.dp)
+                    .padding(end = 8.dp),
+                onImeAction = submit // pass the submit callback to TodoInputText
             )
             TodoEditButton(
-                onClick = {
-                    onItemComplete(TodoItem(text)) // send onItemComplete event up
-                    setText("") // clear the internal text
-                },
+                onClick = submit, // pass the submit callback to TodoEditButton
                 text = "Add",
-                modifier = Modifier.align(Alignment.CenterVertically)
+                modifier = Modifier.align(Alignment.CenterVertically),
+                enabled = text.isNotBlank()
             )
         }
+        if (iconsVisible) {
+            AnimatedIconRow(icon, setIcon, Modifier.padding(top = 8.dp))
+        } else {
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+
     }
 }
 
